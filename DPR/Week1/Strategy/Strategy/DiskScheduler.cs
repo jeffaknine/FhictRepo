293ea9b27181f3f2c7766fcde6afed2717b4aaea
Arthur Zaitve <arthurzaitcev@gmai.com>
2017-02-18 16:26:21 +0000

    t quit

---
+++ DPR/Week1/Strategy/Strategy/DiskScheduler.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Strategy
+{
+   internal class DiskScheduler
+    {
+        private List<int> array;
+        private System.Windows.Forms.Timer timer;
+        private ListBox list;
+        private string current = "";
+        private TextBox box;
+        private Dictionary<string, IStrategy> strategies;
+        private int head; // head current
+        private int temp; // index to remove
+        private Random random;
+        private Label label;
+        private TrackBar trackbar;
+        public DiskScheduler(ListBox list1,TextBox box1,Label label1,TrackBar bar)
+        {
+            random = new Random();
+            list = list1;
+            box = box1;
+            trackbar = bar;
+            label = label1;
+            timer = new System.Windows.Forms.Timer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = 8000;
+            timer.Enabled = true;
+            timer.Stop();
+            array = new List<int>();
+            strategies = new Dictionary<string, IStrategy>();
+            strategies.Add("FIFO", new FIFO());
+            strategies.Add("SSTF", new SSTF());
+            strategies.Add("SCAN", new SCAN());
+            for (int i = 0; i < 10; i++)
+            {
+                
+                addNode();
+              
+            }
+            head = 9;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (current != "")
+            {
+                label.Text = array[head].ToString();
+                temp = strategies[current].findNext(array, ref head);
+                box.Text = array[temp].ToString();
+                trackbar.Value = array[head];
+                removeNode(temp);
+                addNode();
+                
+                box.Text = array[temp].ToString();
+                trackbar.Value = array[head];
+                label.Text = array[head].ToString();
+            }
+
+        }
+
+        public void removeNode(int toRemove)
+        {
+            array.RemoveAt(toRemove);
+            list.Items.RemoveAt(toRemove);
+        }
+
+        public void addNode()
+        {
+            int i = Math.Abs(random.Next(100));
+            array.Add(i);
+            list.Items.Add(i);
+        }
+
+        public void selectStrategy(string strategy)
+        {
+            current = strategy;
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+
+    }
+}
---
+++ DPR/Week1/Strategy/Strategy/SCAN.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class SCAN : IStrategy
+    {
+        static bool i;
+        static bool Checked = false;
+        public int findNext(List<int> list,ref int head)
+        {
+            int half, position, remove = head;
+            position = Math.Abs(list.Count - head);
+            half = list.Count / 2;
+            if(head == 0 || head==(list.Count-1))
+            {
+                Checked = false;
+            }
+            if (Checked == false)
+            {
+                i = (position > half && head != 0) || (head == (list.Count - 1));
+                Checked = true;
+            }            
+            if (i)
+            {
+                head -= 1;
+                return remove;
+            }
+            else
+            {
+                return remove;
+            }
+        }
+    }
+}
